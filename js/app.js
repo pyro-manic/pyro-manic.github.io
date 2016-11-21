c312a85d78fb5f867e2eca121bd6a82d79ab29ba
Yaniv <yaniv.so2@gmail.com>
2016-11-21 19:16:58 +0200

    More Angular

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 const app = angular.module("cv", []); 
 app.controller("contentController", ($scope) => {
-    $scope.languages = cvmodel.languages;
+	$scope.name = cvmodel.name;
+    $scope.languages = cvList(cvmodel.languages);
+    $scope.environments = cvList(cvmodel.environments);
+    $scope.extensions = cvList(cvmodel.extensions);
 });
 
 app.directive("awesomeIcon",() => {
@@ -8,3 +11,7 @@ app.directive("awesomeIcon",() => {
 		template: '<h2><i class="fa fa-code" aria-hidden="true"></i> Languages</h2>'
 	}
 });
+
+let cvList = (arr) => {
+	return arr.join(', ');
+}
--- js/content.js
+++ js/content.js
@@ -1,3 +1,17 @@
 const cvmodel = {
-	languages: ["Java", "C#", "JavaScript(ES6)", "PHP", "HTML5", "CSS3", "SQL", "XML", "JSON", "Python", "Lua"]
+	name: "Yaniv Sobol",
+	languages: ["Java", "C#", "JavaScript (ES6)", "PHP", "HTML5", "CSS3", "SQL", "XML", "JSON", "Python", "Lua"],
+	extensions: ["Bootstrap", "jQuery", "AngularJS", "ReactJS"],
+	environments: ["Android Studio", "Eclipse", "Unity", "MySQL", "Sublime Text 3", "GitHub", "Notepad++", "PyCharm", "Wordpress",
+	"Microsoft Word, Excel", "Adobe Photoshop, Illustrator"],
+	human_languages: ["Hebrew (native tongue)", "Englsh (native level)"]
 }
+
+const modals = [
+	{
+		title: "IPECAC Community Mod",
+	}, 
+	{
+
+	}
+]
