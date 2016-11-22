d72980dcbbfbc60291bb6e95e2c1b51662728522
Yaniv <yaniv.so2@gmail.com>
2016-11-22 11:15:34 +0200

    More data binding

--- js/app.js
+++ js/app.js
@@ -1,16 +1,28 @@
 const app = angular.module("cv", []); 
+
+/** Controllers **/
+
 app.controller("contentController", ($scope) => {
 	$scope.name = cvmodel.name;
     $scope.languages = cvList(cvmodel.languages);
     $scope.environments = cvList(cvmodel.environments);
     $scope.extensions = cvList(cvmodel.extensions);
+
 });
+
+app.controller("modalController", ($scope) => {
+	$scope.modals = cvmodals;
+});
+
+/** Directives **/
 
 app.directive("awesomeIcon",() => {
 	return {
 		template: '<h2><i class="fa fa-code" aria-hidden="true"></i> Languages</h2>'
 	}
 });
+
+/** Helper Functions **/
 
 let cvList = (arr) => {
 	return arr.join(', ');
--- js/content.js
+++ js/content.js
@@ -7,11 +7,10 @@ const cvmodel = {
 	human_languages: ["Hebrew (native tongue)", "Englsh (native level)"]
 }
 
-const modals = [
-	{
+const cvmodals = {
+	ipecac: {
 		title: "IPECAC Community Mod",
-	}, 
-	{
-
+		body: [ `A mod for the 2014 roguelike video game, "The Binding of Isaac: Rebirth". The mod is a collaboration of upwards of ten regular team members worldwide, and also includes sporadic contributions from the game's online community.`
+		, `Duties: Lua programming, game design, writing to the project's Wordpress-based development blog, attending regular team meetings and discussing new features, creating and editing pixel art sprites in Paint.net`]
 	}
-]
+}
