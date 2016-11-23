6c43b9c5ddff0442a58243717cf29dfbb37d446c
Yaniv <yaniv.so2@gmail.com>
2016-11-23 15:00:44 +0200

    Better contact buttons

--- js/app.js
+++ js/app.js
@@ -8,17 +8,20 @@ app.controller("contentController", ($scope) => {
     $scope.environments = cvList(cvmodel.environments);
     $scope.extensions = cvList(cvmodel.extensions);
 
+    $scope.games = cvList(cvbonus.games);
+
 });
 
 app.controller("modalController", ($scope) => {
 	$scope.modals = cvmodals;
-});
+	$scope.terminate = cvmodals.btn_terminate;
+})
 
 /** Directives **/
 
 app.directive("awesomeIcon",() => {
 	return {
-		template: '<h2><i class="fa fa-code" aria-hidden="true"></i> Languages</h2>'
+		template: `<h2><i class="fa fa-code" aria-hidden="true"></i> Languages</h2>`
 	}
 });
 
--- js/content.js
+++ js/content.js
@@ -1,3 +1,4 @@
+
 const cvmodel = {
 	name: "Yaniv Sobol",
 	languages: ["Java", "C#", "JavaScript (ES6)", "PHP", "HTML5", "CSS3", "SQL", "XML", "JSON", "Python", "Lua"],
@@ -8,9 +9,33 @@ const cvmodel = {
 }
 
 const cvmodals = {
+	btn_terminate: `Got it`,
 	ipecac: {
-		title: "IPECAC Community Mod",
+		title: `IPECAC Community Mod`,
 		body: [ `A mod for the 2014 roguelike video game, "The Binding of Isaac: Rebirth". The mod is a collaboration of upwards of ten regular team members worldwide, and also includes sporadic contributions from the game's online community.`
-		, `Duties: Lua programming, game design, writing to the project's Wordpress-based development blog, attending regular team meetings and discussing new features, creating and editing pixel art sprites in Paint.net`]
+		, `Duties: Lua programming, game design, writing to the project's Wordpress-based development blog, attending regular team meetings and discussing new features, creating and editing pixel art sprites in Paint.net`],
+		demo: "Lua, Game Design,"
+	},
+	rick: {
+		title: `Drunk Rick Cipher`,
+		body: `A cipher app that takes text input from the user, and displays it back to the user as if spoken by a drunk Rick of "Rick & Morty" fame.`,
+		demo: []
+	},
+	education: {
+		title: `Education`,
+		body: [
+			`2016 - Mobile Android Application Development, John Bryce Hi-Tech College Tel-Aviv`,
+			`2015-present - PHP, JavaScript, HTML, Python, ReactJS (online courses, self-paced), Codecademy`,
+			`2016-present - jQuery, AngularJS, HTML5, CSS3, W3Schools`
+		]
+	},
+	munch: {
+		title: "Munch",
+		body: "The final project from my Android coding bootcamp. "
 	}
+}
+
+const cvbonus = {
+	games: ['Binding of Isaac', 'Spelunky', 'Half-Life', 'Team Fortress (Classic, 2)', 'Hotline Miami', 'Stardew Valley', 'Super Meat Boy', 'Minecraft' ],
+	music: ['NOFX', 'Descendents', 'Iron Maiden', 'Arctic Monkeys', 'Not On Tour', 'Vitarti', 'Cain and Abel 90210', 'Dick Dale', 'Red Hot Chili Peppers', '88FM', 'Israeli music (inception - today)']
 }
